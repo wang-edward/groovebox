731104046407be7cba1c0030f3860a809e899a23
edward <edawordwang@gmail.com>
2022-04-03 00:21:11 -0400

    gamma integration: pog!!!

--- src/donda.cpp
+++ src/donda.cpp
@@ -1,3 +1,16 @@
+/*
+What is our "Hello world!" app?
+
+An agent orbits around the origin emitting the audio line input. The camera
+view can be switched between a freely navigable keyboard/mouse controlled mode
+and a sphere follow mode.
+
+Requirements:
+2 channels of spatial sound
+2 windows, one front view, one back view
+stereographic rendering
+*/
+
 #include <iostream>
 #include <vector>
 
@@ -21,23 +34,22 @@ struct MyApp : public App {
   uint64_t frameCounter = 0;
   vector<uint16_t> outputMap;
 
-    float vols [4];
-    void onInit() {
-        vols[0] = 0.01f;
-        vols[1] = 100.0f;
-    }
+  float mix_level[4];
 
+  void onInit() {
+      mix_level[0] = 1.;
+      mix_level[1] = 1.;
+    }
+  
   void onSound(AudioIOData &io) override {
     uint64_t numFrames = io.framesPerBuffer();
     if (frameCounter + numFrames > sfs[0].frameCount) {
       numFrames = sfs[0].frameCount - frameCounter;
     }
     // std::cout << frameCounter << std::endl;
-    for (uint16_t i = 0; i < 2; i++) {
-      float* frames = sfs[i].getFrame(frameCounter);
-      std::cout<<*frames<<"\n";
-      *frames *= vols[i];
-      std::cout<<*frames<<"\n\n";      
+    for (uint16_t i = 0; i < 4; i++) {
+      float *frames = sfs[i].getFrame(frameCounter);
+      // *frames *=  mix_level[i];
       memcpy(io.outBuffer(i), frames, numFrames * sizeof(float));
     }
     frameCounter += io.framesPerBuffer();
@@ -49,18 +61,18 @@ struct MyApp : public App {
 
 int main() {
   std::vector<std::string> filenames;
+  filenames.push_back("data/count_new.wav");
   filenames.push_back("data/count.wav");
-  filenames.push_back("data/beat.wav");
-//   filenames.push_back("data/count.wav");
-//   filenames.push_back("data/count.wav");
+  filenames.push_back("data/count.wav");
+  filenames.push_back("data/count.wav");
 
   MyApp app;
 
-  app.outputMap = {0, 1};
-//    app.outputMap[0] = 2 - 1;
-//    app.outputMap[1] = 53 - 1;
-//    app.outputMap[2] = 59 - 1;
-//    app.outputMap[3] = 8 - 1;
+  app.outputMap = {0, 1, 2, 3};
+  //  app.outputMap[0] = 2 - 1;
+  //  app.outputMap[1] = 53 - 1;
+  //  app.outputMap[2] = 59 - 1;
+  //  app.outputMap[3] = 8 - 1;
 
   for (size_t i = 0; i < filenames.size(); i++) {
     if (!app.sfs[i].open(filenames[i].c_str())) {
@@ -77,6 +89,8 @@ int main() {
   float sr = app.sfs[0].sampleRate;
   app.audioDomain()->audioIO().gain(0.5);  // Global output gain.
   app.audioDomain()->configure(sr, AUDIO_BLOCK_SIZE, 4);
+
+
   app.start();
   return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
+/*
+What is our "Hello world!" app?
+
+An agent orbits around the origin emitting the audio line input. The camera
+view can be switched between a freely navigable keyboard/mouse controlled mode
+and a sphere follow mode.
+
+Requirements:
+2 channels of spatial sound
+2 windows, one front view, one back view
+stereographic rendering
+*/
+
 #include <iostream>
 #include <vector>
 
 #include "al/app/al_App.hpp"
-#include "al/sound/al_SoundFile.hpp"
+// #include "al/sound/al_SoundFile.hpp"
 #include "al/sound/al_Speaker.hpp"
+
+#include "Gamma/SamplePlayer.h"
+using namespace gam;
 
 using namespace al;
 using namespace std;
@@ -17,66 +33,47 @@ typedef struct {
 } meters_t;
 
 struct MyApp : public App {
-  SoundFile sfs[4];
-  uint64_t frameCounter = 0;
-  vector<uint16_t> outputMap;
 
-    float vols [4];
-    void onInit() {
-        vols[0] = 0.01f;
-        vols[1] = 100.0f;
+  SamplePlayer<> player1, player2;
+
+  float mix_level[4];
+
+  void onInit() {
+      player1.load("data/count_new.wav");
+
+      // Assign player1's buffer to player2;
+      // this can be called safely in the audio thread.
+      player2.buffer(player1);
+
+      // Make second playback rate slightly higher to create "phasing"
+      player2.rate(2.0);
     }
-
+  
   void onSound(AudioIOData &io) override {
-    uint64_t numFrames = io.framesPerBuffer();
-    if (frameCounter + numFrames > sfs[0].frameCount) {
-      numFrames = sfs[0].frameCount - frameCounter;
-    }
-    // std::cout << frameCounter << std::endl;
-    for (uint16_t i = 0; i < 2; i++) {
-      float* frames = sfs[i].getFrame(frameCounter);
-      std::cout<<*frames<<"\n";
-      *frames *= vols[i];
-      std::cout<<*frames<<"\n\n";      
-      memcpy(io.outBuffer(i), frames, numFrames * sizeof(float));
-    }
-    frameCounter += io.framesPerBuffer();
-    if (frameCounter >= sfs[0].frameCount) {
-      frameCounter = 0;
-    }
+    while(io()){
+			float s = (player1() + player2()) * 0.5;
+			player1.loop();
+			player2.loop();
+			io.out(0) = io.out(1) = s;
+		}
   }
+    
 };
 
 int main() {
   std::vector<std::string> filenames;
+  filenames.push_back("data/count_new.wav");
   filenames.push_back("data/count.wav");
-  filenames.push_back("data/beat.wav");
-//   filenames.push_back("data/count.wav");
-//   filenames.push_back("data/count.wav");
+  filenames.push_back("data/count.wav");
+  filenames.push_back("data/count.wav");
 
   MyApp app;
 
-  app.outputMap = {0, 1};
-//    app.outputMap[0] = 2 - 1;
-//    app.outputMap[1] = 53 - 1;
-//    app.outputMap[2] = 59 - 1;
-//    app.outputMap[3] = 8 - 1;
-
-  for (size_t i = 0; i < filenames.size(); i++) {
-    if (!app.sfs[i].open(filenames[i].c_str())) {
-      std::cout << " Can't open file: " << filenames[i] << std::endl;
-      return -1;
-    } else {
-      std::cout << "Playing file " << i << " : " << filenames[i] << std::endl;
-      std::cout << " -- sr: " << app.sfs[i].sampleRate
-                << " total frames: " << app.sfs[i].frameCount
-                << " channels: " << app.sfs[i].channels << std::endl;
-    }
-  }
-
-  float sr = app.sfs[0].sampleRate;
+  float sr = 44100;
   app.audioDomain()->audioIO().gain(0.5);  // Global output gain.
   app.audioDomain()->configure(sr, AUDIO_BLOCK_SIZE, 4);
+
+
   app.start();
   return 0;
 }
